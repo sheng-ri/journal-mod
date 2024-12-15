f3d77eb97b1c3e2cf0948eb28b34e2772b24d497
BirthCat <sheng_ri@qq.com>
2024-12-15 13:24:41 +0800

    Release 1.0.1

--- src/main/java/top/birthcat/journalmod/cmmon/JournalDataPacket.java
+++ src/main/java/top/birthcat/journalmod/cmmon/JournalDataPacket.java
@@ -10,7 +10,6 @@ import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
 import top.birthcat.journalmod.JournalMod;
 
